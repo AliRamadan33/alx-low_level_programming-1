60dd5bebb0a908ec374853c9231ba39d37d0ab0e
fayez666 <abdalluhfayez420@gmail.com>
2023-03-21 08:50:59 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign -> this func prints the sign of a number.
+ * @n: the num for the check
+ * Return: 1 and prints + if n is greater than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-1');
+	}
+}
