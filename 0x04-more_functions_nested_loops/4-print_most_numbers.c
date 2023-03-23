2f6458338e788e3a5335db328c7c8cb9a1989704
fayez666 <abdalluhfayez420@gmail.com>
2023-03-23 14:03:45 -0700

    4 -task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers -print nums from 0 : 9 not 4,2
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
